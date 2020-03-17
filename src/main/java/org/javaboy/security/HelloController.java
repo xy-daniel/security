ec7ae74055aa466c74eb029bf977e5e5209ebe4c
86178 <1207350458@qq.com>
2020-03-17 11:22:59 +0800

    控制器

--- src/main/java/org/javaboy/security/HelloController.java
+++ src/main/java/org/javaboy/security/HelloController.java
@@ -1,4 +1,14 @@
 package org.javaboy.security;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello Security";
+    }
+
 }
